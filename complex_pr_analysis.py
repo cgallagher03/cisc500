e8f6046150c74cdcf360bde19febdf22298eb43b
Conor Gallagher <conor1.gallagher@hotmail.co.uk>
2025-03-12 00:08:31 -0400

    Work on cluster count visualizations

---
+++ complex_pr_analysis.py
@@ -0,0 +1,5 @@
+# extract complex prs (upper quartile of comments or time to merge), then get their review threads
+
+
+
+# easiest way to do this is probably to fit those threads against the grouped model and manually observe which cluster each one belongs to
